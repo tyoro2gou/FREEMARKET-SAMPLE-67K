f880791540745819eee8b8573e27a257690efd7c
kmasanori <kmasanori0221@gmail.com>
2020-02-01 11:55:13 +0900

    checked error when upload image to S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   # ↓後で必ずfalseに戻す。
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]

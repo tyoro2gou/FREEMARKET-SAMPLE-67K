1fb25b9bcac60f15682b7f6cca80224ab1c5c2bd
kmasanori <kmasanori0221@gmail.com>
2020-02-04 15:42:55 +0900

    register default category
    カテゴリーの初期値をDBに登録しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_04_030640) do
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "item_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "saler_id"
+    t.bigint "buyer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["buyer_id"], name: "index_item_users_on_buyer_id"
+    t.index ["item_id"], name: "index_item_users_on_item_id"
+    t.index ["saler_id"], name: "index_item_users_on_saler_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", limit: 40, null: false
+    t.text "description", null: false
+    t.integer "status", limit: 1, default: 0, null: false
+    t.integer "postage", limit: 1, default: 0, null: false
+    t.integer "region", limit: 1, default: 0, null: false
+    t.integer "shipping_date", limit: 1, default: 0, null: false
+    t.string "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "test_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "item_users", "items"
+  add_foreign_key "item_users", "users", column: "buyer_id"
+  add_foreign_key "item_users", "users", column: "saler_id"
 end

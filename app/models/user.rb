5bf0534b888897d038fe4fa32bfbc855321977b0
kocchan <59197715+kocchan@users.noreply.github.com>
2020-02-04 20:39:18 +0900

    added fields_for

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday_info, presence: true
   has_one :address, dependent: :destroy
-  
+  accepts_nested_attributes_for :address
 end

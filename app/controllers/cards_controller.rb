fda4c8616cd9d04f60a30a710458bc3076935200
101415 <j.chicharito.h.0014@gmail.com>
2020-02-04 16:38:25 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -16,25 +16,29 @@ class CardsController < ApplicationController
     end
 end
 
-  def pay #payjpとCardのデータベース作成を実施します。
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    if params['payjp-token'].blank?
-      redirect_to action: "new"
+def pay #クレジットカード登録
+  Payjp.api_key = "sk_test_fc909327daf398b939d901a1"
+  # ここでテスト鍵をセットしてあげる(環境変数にしても良い)
+  if params['payjpToken'].blank?
+  # paramsの中にjsで作った'payjpTokenが存在するか確かめる
+    redirect_to action: "new"
+  else
+    customer = Payjp::Customer.create(
+    card: params['payjpToken'],
+    metadata: {user_id: current_user.id}
+    )
+   # ↑ここでjay.jpに保存
+    @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+   # ここでdbに保存
+    if @card.save
+      redirect_to action: "show"
+      flash[:notice] = 'クレジットカードの登録が完了しました'
     else
-      customer = Payjp::Customer.create(
-      description: '登録テスト', #なくてもOK
-      email: current_user.email, #なくてもOK
-      card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "show"
-      else
-        redirect_to action: "pay"
-      end
+      redirect_to action: "pay"
+      flash[:alert] = 'クレジットカード登録に失敗しました'
     end
   end
+end
   def delete #PayjpとCardデータベースを削除します
     card = Card.where(user_id: current_user.id).first
     if card.blank?
@@ -46,17 +50,16 @@ end
     end
       redirect_to action: "new"
   end
-
-  def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
+  def show
+    card = current_user.credit_card
     if card.blank?
       redirect_to action: "new" 
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = "sk_test_fc909327daf398b939d901a1"
       customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      @customer_card = customer.cards.retrieve(card.card_id)
     end
-  end
+end
 end
 
   def create

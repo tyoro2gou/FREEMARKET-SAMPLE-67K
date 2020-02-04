d2c12b8023d7d57bcddd7723f156d722ad27c614
101415 <j.chicharito.h.0014@gmail.com>
2020-02-04 13:12:25 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'top#index'
   devise_for :users
   resources :top, only: [:new, :create]
-  resources :users, only: :show
+  resources :users, only: :show do
+    get 'logoutpage'
+  end
   resources :items, only: [:index, :new, :create, :show]
   resources :cards, only: [:index, :new, :create]
 end

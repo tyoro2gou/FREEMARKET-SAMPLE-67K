fe2dbeee21f0201cc09b647d9c6e2ea5a7e78847
kmasanori <kmasanori0221@gmail.com>
2020-02-05 11:26:46 +0900

    item-sale-page front coding & function
    商品出品ページのコーディングと画像アップロード以外のitem情報の登録ができるようにしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :move_to_top
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    # binding.pry
+    @item = Item.new(item_params)
+    # binding.pry
+    if @item.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+  def move_to_top
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :postage_id, :region_id, :shipping_date_id, :price).merge(saler_id: current_user.id)
   end
 end

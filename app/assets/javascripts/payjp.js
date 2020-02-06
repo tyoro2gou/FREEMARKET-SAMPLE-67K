1616387b10480b799abf80ead7bfc06fe6d82496
101415 <j.chicharito.h.0014@gmail.com>
2020-02-06 17:02:30 +0900

    commit

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,35 +1,3 @@
-// $(document).on('turbolinks:load', function() {
-//   var form = $("#inputForm");
-//   Payjp.setPublicKey('pk_test_46e0715c69e5c4ec9239df4c');
-//   $(document).on("click", "#content-bottom--add-btn", function(e) {
-
-//     e.preventDefault();
-//     form.find("input[type=submit]").prop("disabled", true);
-
-//     var card = {
-//         number: $("#payment_card_no").val(),
-//         cvc: $("#payment_card_cvc").val(),
-//         exp_month: $("#payment_expiration_date_1i").val(),
-//         exp_year: $("#payment_expiration_date_2i").val(),
-//     };
-//     Payjp.createToken(card, function(s, response) {
-//       if (response.error) {
-//         alert('トークン作成エラー発生');
-//       }
-//       else {
-//         $(".number").removeAttr("name");
-//         $(".cvc").removeAttr("name");
-//         $(".exp_month").removeAttr("name");
-//         $(".exp_year").removeAttr("name");
-//         var token = response.id;
-
-//         form.append($('<input type="hidden" name="payjpToken" />').val(token));
-//         form.get(0).submit();
-//       }
-//     });
-//   });
-// });
-
 document.addEventListener(
   "DOMContentLoaded", (e) => {
     Payjp.setPublicKey("pk_test_8a02eb526da8e8fce8bd4e8c");
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,61 +1,3 @@
-# class CardsController < ApplicationController
-#   require "payjp"
-
-#   def new
-#   end
-
-#   def create
-#     if params['payjp-token'].blank?
-#       redirect_to action: "index", id: current_user.id
-#     else
-#       customer = Payjp::Customer.create(
-#       email: current_user.email,
-#       card: params['payjp-token'],
-#       metadata: {user_id: current_user.id}
-#       )
-#       @card = Cards.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-#       if @card.save!
-#         redirect_to action: "show"
-#       else
-#         redirect_to action: "new", id: current_user.id
-#       end
-#     end
-#   end
-
-#   def delete
-#     card = current_user.credit_cards.first
-#     if card.present?
-#       customer = Payjp::Customer.retrieve(card.customer_id)
-#       customer.delete
-#       card.delete
-#     end
-#       redirect_to action: "confirmation", id: current_user.id
-#   end
-
-#   def show
-#     card = current_user.cards.first
-#     if card.present?
-#       customer = Payjp::Customer.retrieve(card.customer_id)
-#       @default_card_information = customer.cards.retrieve(card.card_id)
-#     else
-#       redirect_to action: "index", id: current_user.id
-#     end
-#   end
-#   def index
-#       card = current_user.cards
-#       redirect_to action: "show" if card.exists?
-#   end
-#   private
-
-#   def get_payjp_info
-#     if Rails.env == 'development'
-#       Payjp.api_key = ENV["pk_test_8a02eb526da8e8fce8bd4e8c"]
-#     else
-#       Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
-#     end
-#   end
-# end
-
 class CardsController < ApplicationController
   require "payjp"
   before_action :set_card
@@ -69,18 +11,16 @@ class CardsController < ApplicationController
     Payjp.api_key = 'sk_test_fc909327daf398b939d901a1'
 
     if params['payjp-token'].blank?
-      redirect_to action: "index"
-    
+      redirect_to new_card_path
     else
        customer = Payjp::Customer.create(
-        # email: current_user.email,
         card: params['payjp-token'], 
        )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "new"
+        redirect_to new_card_path
       else
-        redirect_to action: "create"
+        redirect_to cards_path
       end
     end
   end

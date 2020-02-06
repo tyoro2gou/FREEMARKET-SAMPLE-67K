d34f9608e2d8b500805e449d6fff081593a00535
kmasanori <kmasanori0221@gmail.com>
2020-02-06 11:18:34 +0900

    front-coding mypage
    マイページのコーディングを行いました。
    クレジット登録画面は、クレジット登録機能と合わせて実装中です。

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :top, only: [:new, :create]
   resources :users, only: :show do
-    get 'logoutpage'
+    collection do
+      get 'before_logout'
+    end
   end
-  resources :items, only: [:index, :new, :create, :show]
-  resources :cards, only: [:index, :new, :show] do 
+  resources :items, only: [:index, :new, :create, :show] do
+    collection do
+      get 'saling_show'
+      get 'saled_show'
+    end
+  end
+  resources :cards, only: [:index, :new, :show] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'

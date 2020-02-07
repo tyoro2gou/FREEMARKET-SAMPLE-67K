608ab7b652577633565990ae3c67071ccb9e1b25
kmasanori <kmasanori0221@gmail.com>
2020-02-07 12:27:59 +0900

    show address in my page profile
    住所をマイページのプロフィール欄にて表示させました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :move_to_show, only: :destroy
   def show
     @user = User.find(current_user.id)
+    @address = Address.find_by(user_id: current_user.id)
   end
 
   def before_logout
@@ -9,5 +10,5 @@ class UsersController < ApplicationController
 
   # Payjp.api_key = PAYJP_SECRET_KEY = 'sk_test_fc909327daf398b939d901a1'
   # customer = Payjp::Customer.create(card: params[:payjp_token])
- 
+
 end

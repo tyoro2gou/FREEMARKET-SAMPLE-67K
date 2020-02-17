7acb07201f4b65d9b3a47b039ebed71e603e76b1
kocchan <59197715+kocchan@users.noreply.github.com>
2020-02-17 19:50:10 +0900

    バリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :name, :description, :status_id, :postage_id, :region_id, :shipping_date_id, :price, presence: true 
+  validates :name, :description, :price, presence: true 
+  validates :status_id, inclusion: { in: %w(0), message: "商品の状態を選択してください" }
+  validates :postage_id, inclusion: { in: %w(0), message: "配送料の負担を選択してください" }
+  validates :region_id, inclusion: { in: %w(0), message: "発送元の地域を選択してください" }
+  validates :shipping_date_id, inclusion: { in: %w(0), message: "発送までの日数を選択してください" }
   has_many :comments
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true

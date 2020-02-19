244bff5dc27e7c7723dea41fdf097018881a68f1
tyoro2gou <asd4asd74@yahoo.co.jp>
2020-02-19 19:12:44 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -49,17 +49,6 @@ ActiveRecord::Schema.define(version: 2020_02_05_091717) do
     t.index ["item_id"], name: "index_images_on_item_id"
   end
 
-  create_table "item_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "saler_id"
-    t.bigint "buyer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["buyer_id"], name: "index_item_users_on_buyer_id"
-    t.index ["item_id"], name: "index_item_users_on_item_id"
-    t.index ["saler_id"], name: "index_item_users_on_saler_id"
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 40, null: false
     t.text "description", null: false
